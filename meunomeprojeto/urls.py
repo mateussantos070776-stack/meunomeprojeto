19cd9411ea6ff98416ad8a60ad98eac90c2cd177
mateus <mateussantos070776@gmail.com>
2026-02-20 22:33:32 -0300

    Revert "Login admin via card"
    This reverts commit 6fffbfbdafb4bc7f6f773953f83b551192c0b86e.

--- meunomeprojeto/urls.py
+++ meunomeprojeto/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from paginas.views import admin_login, home
+from paginas.views import home 
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path('admin-login/', admin_login),
     path('', home), 
 ]
